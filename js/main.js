aeb8b1c4871805b54da8b4bc0dae5cd3a368846f
Isadora Zamora <isadora.zamora.f@mail.pucv.cl>
2025-04-29 11:26:10 -0400

    contenido de las páginas

--- js/main.js
+++ js/main.js
@@ -19,3 +19,27 @@ openMenuButton.addEventListener('click', () => {
 closeMenuButton.addEventListener('click', () => {
   menu.classList.remove('menu-open'); // Quitamos la clase del menú
 });
+
+function mostrarCategoria(categoria) {
+  // Primero ocultamos todos los proyectos
+  const proyectos = document.querySelectorAll('.categoria-proyecto');
+  proyectos.forEach(proyecto => {
+    proyecto.style.display = 'none';
+  });
+
+  // Mostramos solo los proyectos de la categoría seleccionada
+  const proyectosCategoria = document.querySelectorAll(`.categoria-proyecto.${categoria}`);
+  proyectosCategoria.forEach(proyecto => {
+    proyecto.style.display = 'block';
+  });
+}
+
+// Cambiar el texto del botón cuando se haga click
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', function() {
+    const target = document.querySelector(this.getAttribute('data-bs-target'));
+    if (target.classList.contains('collapse')) {
+      this.textContent = target.classList.contains('show') ? 'Ver detalles' : 'Ocultar detalles';
+    }
+  });
+});
